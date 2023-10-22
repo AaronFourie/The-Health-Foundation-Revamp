a7fc05ecd3b560363fb5756babafc07b8d512067
AaronFourie <ST10101067@rccoennect.edu.za>
2023-10-22 19:33:28 +0200

    Firebase Login, Register and Logout fully functional

--- Chaotic Nature XISD6329/Program.cs
+++ Chaotic Nature XISD6329/Program.cs
@@ -1,8 +1,16 @@
+using Microsoft.Extensions.Configuration;
+using FirebaseAdmin;
+using Google.Apis.Auth.OAuth2;
 using Chaotic_Nature_XISD6329.Data;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
+using FirebaseAdmin.Auth;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Load configuration from appsettings.json
+builder.Configuration.AddJsonFile("appsettings.json");
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
@@ -10,9 +18,21 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
+
+// Load Firebase Admin SDK configuration from your JSON file
+var firebaseApp = FirebaseApp.Create(new AppOptions
+{
+    Credential = GoogleCredential.FromFile("Config/wilapp-ec2d3-firebase-adminsdk-wujb2-3d778f7bc9.json"),
+});
+// Add Firebase Authentication
+builder.Services.AddScoped(_ =>
+{
+    return FirebaseAuth.DefaultInstance;
+});
+
 
 var app = builder.Build();
 
@@ -24,7 +44,6 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -32,7 +51,6 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
 app.UseAuthorization();
 
 app.MapControllerRoute(
